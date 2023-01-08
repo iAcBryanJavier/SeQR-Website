696c9e7198b2e35eb107b29c4f3a775de0a9fc74
Bryan Christian Javier <bryanj@titusgt.com>
2023-01-08 17:58:31 +0800

    #2 navbar converted to Angular CLI

---
+++ SeQR-Website/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserNavbarComponent } from './user-navbar/user-navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserNavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    NavbarComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ SeQR-Website/src/app/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  standalone: true,
+	imports: [NgbCollapseModule, RouterLink]
+})
+export class NavbarComponent {
+  isMenuCollapsed = true;
+}
