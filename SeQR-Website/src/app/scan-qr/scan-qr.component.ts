24163165a39bc5780b46bc7fb464fd1aa3d9bc13
unknown <201901189@iacademy.edu.ph>
2023-01-10 16:51:21 +0800

    Added QR Code Scanner
    The library is ngx-scanner resources are
    
    Github: https://github.com/zxing-js/ngx-scanner/wiki \n NPM: https://zxing-js.github.io/ngx-scanner/ \n NOTE: STAY ON VER 3.6.2 or else component will break.

--- SeQR-Website/src/app/scan-qr/scan-qr.component.ts
+++ SeQR-Website/src/app/scan-qr/scan-qr.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-scan-qr',
   templateUrl: './scan-qr.component.html',
-  styleUrls: ['./scan-qr.component.css']
+  styleUrls: ['./scan-qr.component.css'],
+  standalone: true,
+	imports: [ZXingScannerModule]
 })
-export class ScanQrComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ScanQrComponent  {
+  scannerEnabled = false;
+  
 }
