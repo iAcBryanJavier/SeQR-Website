2f6ccd0031ca08aa9628e42e2f7b3121d6209c25
Bryan Christian Javier <bryanj@titusgt.com>
2023-01-11 14:19:40 +0800

    fixed app navbar component format

--- SeQR-Website/src/app/scan-qr/scan-qr.component.ts
+++ SeQR-Website/src/app/scan-qr/scan-qr.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
-  selector: 'app-scan-qr',
-  templateUrl: './scan-qr.component.html',
-  styleUrls: ['./scan-qr.component.css'],
-  standalone: true,
-	imports: [ZXingScannerModule]
+    selector: 'app-scan-qr',
+    templateUrl: './scan-qr.component.html',
+    styleUrls: ['./scan-qr.component.css'],
+    standalone: true,
+    imports: [ZXingScannerModule, NavbarComponent]
 })
 export class ScanQrComponent  {
   scannerEnabled = false;
