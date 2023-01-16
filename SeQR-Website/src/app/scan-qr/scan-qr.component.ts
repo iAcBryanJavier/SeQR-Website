3936f3deb37cf9540d715f6b8d9459c385a27aae
Carl Mercado <53513555+NaldevCX@users.noreply.github.com>
2023-01-16 15:17:57 +0800

    Added html5qrcode code
    test

--- SeQR-Website/src/app/scan-qr/scan-qr.component.ts
+++ SeQR-Website/src/app/scan-qr/scan-qr.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-scan-qr',
   templateUrl: './scan-qr.component.html',
   styleUrls: ['./scan-qr.component.css'],
-  standalone: true,
-	imports: [ZXingScannerModule]
+
 })
 export class ScanQrComponent  {
-  scannerEnabled = false;
+
   
 }
